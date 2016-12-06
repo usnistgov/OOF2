02ef0473a979e4da059a308a0683b8d9a48e11ae
Steve Langer <stephen.langer@nist.gov>
2016-12-06 15:29:27 -0500

    Fixed some comments.

--- SRC/engine/preconditioner.py
+++ SRC/engine/preconditioner.py
@@ -1,6 +1,21 @@
+# -*- python -*-
+
+# This software was produced by NIST, an agency of the U.S. government,
+# and by statute is not subject to copyright in the United States.
+# Recipients of this software assume all responsibilities associated
+# with its operation, modification and maintenance. However, to
+# facilitate maintenance we ask that before distributing modified
+# versions of this software, you first contact the authors at
+# oof_manager@nist.gov. 
+
 from ooflib.common import registeredclass
 from ooflib.common.IO import parameter
 from ooflib.common.IO import xmlmenudump
+
+## In the Eigen matrix method templates, the preconditioner isn't a
+## separate object from the solver.  These classes here are just
+## placeholders that are used in the PreconditionedMatrixMethod
+## classes in matrixmethod.py to choose the correct Eigen routine.
 
 class PreconditionerBase(registeredclass.RegisteredClass):
     registry = []
