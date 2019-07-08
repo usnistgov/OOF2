5817e3826d86255ba22ebcd4d9eee4dc50b84ab7
Steve Langer <stephen.langer@nist.gov>
2019-07-07 22:20:30 -0400

    Moved the Burn PixelSelectionMethod in pixelselectionmethod.py.
    It had ended up in autograin.spy after all of the refactoring
    induced by adding the AutoGrain method, but that didn't make
    sense in the end.  autograin.* will be renamed next.

--- SRC/image/pixelselectionmethod.py
+++ SRC/image/pixelselectionmethod.py
@@ -9,10 +9,13 @@
 # oof_manager@nist.gov.
 
 
+from ooflib.SWIG.image import autograin
 from ooflib.SWIG.image import pixelselectioncourieri
+from ooflib.common import enum
 from ooflib.common import pixelselectionmethod
 from ooflib.common import primitives
 from ooflib.common.IO import colordiffparameter
+from ooflib.common.IO import parameter
 from ooflib.common.IO import xmlmenudump
 
 class ColorSelector(pixelselectionmethod.SelectionMethod):
@@ -33,10 +36,63 @@ pixelselectionmethod.PixelSelectionRegistration(
     ColorSelector,
     ordering=0.6,
     events=['up'],
-    params=[colordiffparameter.ColorDifferenceParameter('range',
-                                                        tip='Acceptable deviation from the reference color.')],
+    params=[
+        colordiffparameter.ColorDifferenceParameter(
+            'range',
+            tip='Acceptable deviation from the reference color.')
+    ],
     whoclasses=['Image'],
     tip="Select pixels whose color is close to that of a reference pixel.",
     discussion=xmlmenudump.loadFile('DISCUSSIONS/image/reg/colorselect.xml')
     )
+
+#=--=##=--=##=--=##=--=##=--=##=--=##=--=##=--=##=--=##=--=##=--=##=--=#
+
+BurnSelection = pixelselectioncourieri.BurnSelection
+
+class Burn(pixelselectionmethod.SelectionMethod):
+    def __init__(self, local_flammability, global_flammability,
+                 color_space_norm,
+                 next_nearest):
+        self.local_flammability = local_flammability
+        self.global_flammability = global_flammability
+        self.color_space_norm = color_space_norm
+        self.next_nearest = next_nearest
+    def select(self, immidge, points, selector):
+        ms = immidge.getMicrostructure()
+        startpt = ms.pixelFromPoint(points[0])
+        if immidge.getSelectionContext().getObject().checkpixel(startpt):
+            cd = autograin.ColorDifferentiator(immidge.path(),
+                                     self.local_flammability,
+                                     self.global_flammability,
+                                     self.color_space_norm=="L2")
+            selector(BurnSelection(ms, cd.cobj, startpt, self.next_nearest))
+            # b = BasicBurner(self.local_flammability, self.global_flammability,
+            #                 self.color_space_norm==L2, self.next_nearest)
+            # selector(BurnSelection(ms, b, immidge.getObject(), startpt))
+
+
+pixelselectionmethod.PixelSelectionRegistration(
+    'Burn',
+    Burn,
+    ordering=1.0,
+    params=[
+    parameter.FloatRangeParameter(
+        'local_flammability',
+        range=(0, 1, 0.001), value=0.1,
+        tip='Maximum difference in neighboring pixel values across which a burn will extend.'),
+    parameter.FloatRangeParameter(
+        'global_flammability',
+        range=(0, 1, 0.001), value=0.2,
+        tip='Difference from initial pixel value beyond which a burn will not spread.'),
+    enum.EnumParameter(
+        'color_space_norm', autograin.ColorNorm, value=autograin.L1,
+        tip="How to compute the difference between two colors in RGB space."),
+    parameter.BooleanParameter(
+        'next_nearest', value=0,
+        tip="Burn next-nearest neighbors?")],
+    events=['up'],
+    whoclasses=['Image'],
+    tip="Select a contiguous set of similar pixels, using a forest fire algorithm.",
+    discussion=xmlmenudump.loadFile('DISCUSSIONS/image/reg/burn.xml'))
 
