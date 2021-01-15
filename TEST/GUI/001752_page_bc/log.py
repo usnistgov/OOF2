80947378eb2cdb46633dddce1ffde5cf9bb12295
Steve Langer <stephen.langer@nist.gov>
2021-01-15 12:18:10 -0500

    Added a couple more boundary condition page tests.

---
+++ TEST/GUI/001752_page_bc/log.py
@@ -0,0 +1,329 @@
+checkpoint toplevel widget mapped OOF2
+checkpoint page installed Introduction
+checkpoint toplevel widget mapped OOF2 Activity Viewer
+# ## This file tests for the absence of a bug that was causing KeyErrors
+## when switching to the Boundary Condition page after editing a
+## Subproblem.
+
+# This test was 00178_page_bc before the gtk3 upgrade
+
+import tests
+
+findWidget('OOF2:FE Mesh Page:Pane').set_position(557)
+findWidget('OOF2:FE Mesh Page:Pane:leftpane').set_position(106)
+findWidget('OOF2').resize(782, 511)
+findWidget('OOF2:Navigation:Next').clicked()
+checkpoint page installed Microstructure
+checkpoint meshable button set
+checkpoint microstructure page sensitized
+findWidget('OOF2:Microstructure Page:Pane').set_position(184)
+findWidget('OOF2:Microstructure Page:NewFromFile').clicked()
+checkpoint toplevel widget mapped Dialog-Load Image and create Microstructure
+findWidget('Dialog-Load Image and create Microstructure').resize(237, 200)
+findWidget('OOF2').resize(782, 545)
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('e')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('ex')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('exa')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('exam')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('examp')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('exampl')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('example')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('examples')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('examples/')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('examples/s')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('examples/sm')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('examples/sma')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('examples/smal')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('examples/small')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('examples/small.')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('examples/small.p')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('examples/small.pp')
+findWidget('Dialog-Load Image and create Microstructure:filename').set_text('examples/small.ppm')
+findWidget('Dialog-Load Image and create Microstructure:widget_GTK_RESPONSE_OK').clicked()
+checkpoint meshable button set
+checkpoint microstructure page sensitized
+checkpoint pixel page updated
+checkpoint pixel page sensitized
+checkpoint active area status updated
+checkpoint mesh bdy page updated
+findWidget('OOF2:Microstructure Page:Pane').set_position(189)
+checkpoint Field page sensitized
+checkpoint Materials page updated
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page sensitized
+checkpoint boundary page updated
+checkpoint skeleton selection page grouplist Element
+checkpoint skeleton selection page selection sensitized Element
+checkpoint skeleton selection page updated
+checkpoint skeleton selection page groups sensitized Element
+checkpoint Solver page sensitized
+checkpoint microstructure page sensitized
+checkpoint OOF.Microstructure.Create_From_ImageFile
+findWidget('OOF2:Navigation:Next').clicked()
+checkpoint page installed Image
+findWidget('OOF2:Image Page:Pane').set_position(546)
+findWidget('OOF2:Image Page:Group').clicked()
+checkpoint toplevel widget mapped Dialog-AutoGroup
+findWidget('Dialog-AutoGroup').resize(207, 92)
+findWidget('Dialog-AutoGroup:widget_GTK_RESPONSE_OK').clicked()
+checkpoint meshable button set
+checkpoint meshable button set
+checkpoint microstructure page sensitized
+checkpoint skeleton selection page groups sensitized Element
+checkpoint microstructure page sensitized
+checkpoint meshable button set
+checkpoint meshable button set
+checkpoint microstructure page sensitized
+checkpoint OOF.Image.AutoGroup
+event(Gdk.EventType.BUTTON_PRESS,x= 7.4000000000000e+01,y= 1.9000000000000e+01,button=1,state=0,window=findWidget('OOF2:Navigation:PageMenu').get_window())
+checkpoint toplevel widget mapped chooserPopup-PageMenu
+findMenu(findWidget('chooserPopup-PageMenu'), ['Skeleton']).activate() # MenuItemLogger
+checkpoint page installed Skeleton
+deactivatePopup('chooserPopup-PageMenu') # MenuItemLogger
+checkpoint skeleton page sensitized
+findWidget('OOF2:Skeleton Page:Pane').set_position(417)
+checkpoint skeleton page sensitized
+findWidget('OOF2:Skeleton Page:New').clicked()
+checkpoint toplevel widget mapped Dialog-New skeleton
+findWidget('Dialog-New skeleton').resize(346, 254)
+findWidget('Dialog-New skeleton:x_elements').set_text('')
+findWidget('Dialog-New skeleton:x_elements').set_text('1')
+findWidget('Dialog-New skeleton:x_elements').set_text('10')
+findWidget('Dialog-New skeleton:y_elements').set_text('')
+findWidget('Dialog-New skeleton:y_elements').set_text('1')
+findWidget('Dialog-New skeleton:y_elements').set_text('10')
+findWidget('Dialog-New skeleton:widget_GTK_RESPONSE_OK').clicked()
+checkpoint skeleton selection page groups sensitized Element
+checkpoint skeleton selection page groups sensitized Element
+checkpoint skeleton selection page groups sensitized Element
+checkpoint skeleton selection page selection sensitized Element
+checkpoint skeleton selection page updated
+checkpoint skeleton selection page groups sensitized Element
+checkpoint skeleton page sensitized
+checkpoint mesh bdy page updated
+checkpoint Field page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page sensitized
+checkpoint boundary page updated
+checkpoint skeleton selection page grouplist Element
+checkpoint skeleton page info updated
+checkpoint skeleton selection page selection sensitized Element
+checkpoint Solver page sensitized
+checkpoint OOF.Skeleton.New
+checkpoint skeleton selection page groups sensitized Element
+checkpoint skeleton page info updated
+checkpoint skeleton selection page updated
+checkpoint skeleton page sensitized
+event(Gdk.EventType.BUTTON_PRESS,x= 2.4000000000000e+01,y= 1.3000000000000e+01,button=1,state=0,window=findWidget('OOF2:Navigation:PageMenu').get_window())
+checkpoint toplevel widget mapped chooserPopup-PageMenu
+findMenu(findWidget('chooserPopup-PageMenu'), ['FE Mesh']).activate() # MenuItemLogger
+checkpoint mesh page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page sensitized
+checkpoint page installed FE Mesh
+deactivatePopup('chooserPopup-PageMenu') # MenuItemLogger
+findWidget('OOF2:FE Mesh Page:Pane:leftpane').set_position(130)
+findWidget('OOF2:FE Mesh Page:New').clicked()
+checkpoint toplevel widget mapped Dialog-Create a new mesh
+findWidget('Dialog-Create a new mesh').resize(299, 244)
+findWidget('Dialog-Create a new mesh:widget_GTK_RESPONSE_OK').clicked()
+checkpoint mesh bdy page updated
+checkpoint mesh bdy page updated
+checkpoint Field page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint OOF.Mesh.New
+# Create a pixel group subproblem
+findWidget('OOF2:FE Mesh Page:Pane:leftpane:Subproblems:New').clicked()
+checkpoint toplevel widget mapped Dialog-Create a new subproblem
+findWidget('Dialog-Create a new subproblem').resize(206, 133)
+event(Gdk.EventType.BUTTON_PRESS,x= 6.9000000000000e+01,y= 1.2000000000000e+01,button=1,state=0,window=findWidget('Dialog-Create a new subproblem:subproblem:RCFChooser').get_window())
+checkpoint toplevel widget mapped chooserPopup-RCFChooser
+findMenu(findWidget('chooserPopup-RCFChooser'), ['PixelGroup']).activate() # MenuItemLogger
+deactivatePopup('chooserPopup-RCFChooser') # MenuItemLogger
+findWidget('Dialog-Create a new subproblem').resize(246, 164)
+event(Gdk.EventType.BUTTON_PRESS,x= 5.4000000000000e+01,y= 1.4000000000000e+01,button=1,state=0,window=findWidget('Dialog-Create a new subproblem:subproblem:PixelGroup:group').get_window())
+checkpoint toplevel widget mapped chooserPopup-group
+findMenu(findWidget('chooserPopup-group'), ['#f800f8']).activate() # MenuItemLogger
+deactivatePopup('chooserPopup-group') # MenuItemLogger
+findWidget('Dialog-Create a new subproblem:widget_GTK_RESPONSE_OK').clicked()
+checkpoint mesh page subproblems sensitized
+checkpoint Solver page sensitized
+checkpoint OOF.Subproblem.New
+# Set fields and equations on the subproblem
+findWidget('OOF2:Navigation:Next').clicked()
+checkpoint Field page sensitized
+checkpoint Field page sensitized
+checkpoint page installed Fields & Equations
+findWidget('OOF2:Fields & Equations Page:HPane').set_position(470)
+event(Gdk.EventType.BUTTON_PRESS,x= 3.2000000000000e+01,y= 1.8000000000000e+01,button=1,state=0,window=findWidget('OOF2:Fields & Equations Page:SubProblem').get_window())
+checkpoint toplevel widget mapped chooserPopup-SubProblem
+findMenu(findWidget('chooserPopup-SubProblem'), ['subproblem']).activate() # MenuItemLogger
+checkpoint Field page sensitized
+deactivatePopup('chooserPopup-SubProblem') # MenuItemLogger
+findWidget('OOF2:Fields & Equations Page:HPane:Fields:Temperature defined').clicked()
+checkpoint Field page sensitized
+checkpoint Field page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint Solver page sensitized
+checkpoint OOF.Subproblem.Field.Define
+findWidget('OOF2:Fields & Equations Page:HPane:Fields:Temperature active').clicked()
+checkpoint Field page sensitized
+checkpoint Field page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint OOF.Subproblem.Field.Activate
+findWidget('OOF2:Fields & Equations Page:HPane:Fields:Temperature in-plane').clicked()
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint OOF.Mesh.Field.In_Plane
+findWidget('OOF2:Fields & Equations Page:HPane:Equations:Heat_Eqn active').clicked()
+checkpoint Field page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint OOF.Subproblem.Equation.Activate
+# Create a boundary condition
+event(Gdk.EventType.BUTTON_PRESS,x= 9.8000000000000e+01,y= 2.0000000000000e+01,button=1,state=0,window=findWidget('OOF2:Navigation:PageMenu').get_window())
+checkpoint toplevel widget mapped chooserPopup-PageMenu
+findMenu(findWidget('chooserPopup-PageMenu'), ['Boundary Conditions']).activate() # MenuItemLogger
+checkpoint page installed Boundary Conditions
+deactivatePopup('chooserPopup-PageMenu') # MenuItemLogger
+findWidget('OOF2:Boundary Conditions Page:Condition:New').clicked()
+checkpoint toplevel widget mapped Dialog-New Boundary Condition
+findWidget('Dialog-New Boundary Condition').resize(353, 330)
+findWidget('Dialog-New Boundary Condition:widget_GTK_RESPONSE_OK').clicked()
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint OOF.Mesh.Boundary_Conditions.New
+# Edit the subproblem
+event(Gdk.EventType.BUTTON_PRESS,x= 4.7000000000000e+01,y= 1.7000000000000e+01,button=1,state=0,window=findWidget('OOF2:Navigation:PageMenu').get_window())
+checkpoint toplevel widget mapped chooserPopup-PageMenu
+findMenu(findWidget('chooserPopup-PageMenu'), ['FE Mesh']).activate() # MenuItemLogger
+checkpoint mesh page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page sensitized
+checkpoint page installed FE Mesh
+deactivatePopup('chooserPopup-PageMenu') # MenuItemLogger
+findWidget('OOF2:FE Mesh Page:Pane:leftpane:Subproblems:Edit').clicked()
+checkpoint toplevel widget mapped Dialog-Edit Subproblem definition
+findWidget('Dialog-Edit Subproblem definition').resize(243, 128)
+event(Gdk.EventType.BUTTON_PRESS,x= 6.6000000000000e+01,y= 1.3000000000000e+01,button=1,state=0,window=findWidget('Dialog-Edit Subproblem definition:subproblem:PixelGroup:group').get_window())
+checkpoint toplevel widget mapped chooserPopup-group
+findMenu(findWidget('chooserPopup-group'), ['#00fc00']).activate() # MenuItemLogger
+deactivatePopup('chooserPopup-group') # MenuItemLogger
+findWidget('Dialog-Edit Subproblem definition').resize(245, 128)
+findWidget('Dialog-Edit Subproblem definition:widget_GTK_RESPONSE_OK').clicked()
+checkpoint Field page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint Solver page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint OOF.Subproblem.Edit
+findWidget('OOF2:FE Mesh Page:Pane:leftpane:Subproblems:Edit').clicked()
+checkpoint toplevel widget mapped Dialog-Edit Subproblem definition
+findWidget('Dialog-Edit Subproblem definition').resize(245, 128)
+event(Gdk.EventType.BUTTON_PRESS,x= 3.2000000000000e+01,y= 1.5000000000000e+01,button=1,state=0,window=findWidget('Dialog-Edit Subproblem definition:subproblem:PixelGroup:group').get_window())
+checkpoint toplevel widget mapped chooserPopup-group
+findMenu(findWidget('chooserPopup-group'), ['#f800f8']).activate() # MenuItemLogger
+deactivatePopup('chooserPopup-group') # MenuItemLogger
+findWidget('Dialog-Edit Subproblem definition:widget_GTK_RESPONSE_OK').clicked()
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint Solver page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint OOF.Subproblem.Edit
+event(Gdk.EventType.BUTTON_PRESS,x= 1.1000000000000e+02,y= 1.0000000000000e+01,button=1,state=0,window=findWidget('OOF2:Navigation:PageMenu').get_window())
+checkpoint toplevel widget mapped chooserPopup-PageMenu
+findMenu(findWidget('chooserPopup-PageMenu'), ['Boundary Conditions']).activate() # MenuItemLogger
+deactivatePopup('chooserPopup-PageMenu') # MenuItemLogger
+checkpoint page installed Boundary Conditions
+findMenu(findWidget('OOF2:MenuBar'), ['File', 'Save', 'Python_Log']).activate()
+checkpoint toplevel widget mapped Dialog-Python_Log
+findWidget('Dialog-Python_Log').resize(192, 122)
+findWidget('Dialog-Python_Log:filename').set_text('s')
+findWidget('Dialog-Python_Log:filename').set_text('se')
+findWidget('Dialog-Python_Log:filename').set_text('ses')
+findWidget('Dialog-Python_Log:filename').set_text('sess')
+findWidget('Dialog-Python_Log:filename').set_text('sessi')
+findWidget('Dialog-Python_Log:filename').set_text('sessio')
+findWidget('Dialog-Python_Log:filename').set_text('session')
+findWidget('Dialog-Python_Log:filename').set_text('session.')
+findWidget('Dialog-Python_Log:filename').set_text('session.l')
+findWidget('Dialog-Python_Log:filename').set_text('session.lo')
+findWidget('Dialog-Python_Log:filename').set_text('session.log')
+findWidget('Dialog-Python_Log:widget_GTK_RESPONSE_OK').clicked()
+checkpoint OOF.File.Save.Python_Log
+assert tests.filediff("session.log")
+findMenu(findWidget('OOF2:MenuBar'), ['File', 'Quit']).activate()
