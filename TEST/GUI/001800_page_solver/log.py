c979762ef07cbb81475f80941886cf9d84d48c37
Steve Langer <stephen.langer@nist.gov>
2021-01-15 18:28:05 -0500

    First GUI test for the Solver page.

---
+++ TEST/GUI/001800_page_solver/log.py
@@ -0,0 +1,300 @@
+# -*- python -*-
+
+# This software was produced by NIST, an agency of the U.S. government,
+# and by statute is not subject to copyright in the United States.
+# Recipients of this software assume all responsibilities associated
+# with its operation, modification and maintenance. However, to
+# facilitate maintenance we ask that before distributing modified
+# versions of this software, you first contact the authors at
+# oof_manager@nist.gov. 
+
+# Test the sensitization of the Solve button as a problem is constructed.
+
+import tests
+
+findWidget('OOF2:FE Mesh Page:Pane').set_position(557)
+findWidget('OOF2:FE Mesh Page:Pane:leftpane').set_position(106)
+checkpoint toplevel widget mapped OOF2
+checkpoint page installed Introduction
+findWidget('OOF2').resize(782, 545)
+event(Gdk.EventType.BUTTON_PRESS,x= 8.3000000000000e+01,y= 1.5000000000000e+01,button=1,state=0,window=findWidget('OOF2:Navigation:PageMenu').get_window())
+checkpoint toplevel widget mapped chooserPopup-PageMenu
+findMenu(findWidget('chooserPopup-PageMenu'), ['Solver']).activate() # MenuItemLogger
+deactivatePopup('chooserPopup-PageMenu') # MenuItemLogger
+checkpoint Solver page sensitized
+checkpoint page installed Solver
+findWidget('OOF2:Solver Page:VPane').set_position(170)
+assert tests.sensitive(False)
+
+event(Gdk.EventType.BUTTON_PRESS,x= 1.0100000000000e+02,y= 1.1000000000000e+01,button=1,state=0,window=findWidget('OOF2:Navigation:PageMenu').get_window())
+checkpoint toplevel widget mapped chooserPopup-PageMenu
+findMenu(findWidget('chooserPopup-PageMenu'), ['Microstructure']).activate() # MenuItemLogger
+deactivatePopup('chooserPopup-PageMenu') # MenuItemLogger
+checkpoint page installed Microstructure
+findWidget('OOF2:Microstructure Page:Pane').set_position(235)
+checkpoint meshable button set
+checkpoint microstructure page sensitized
+findWidget('OOF2:Microstructure Page:Pane').set_position(184)
+findWidget('OOF2:Microstructure Page:New').clicked()
+checkpoint toplevel widget mapped Dialog-Create Microstructure
+findWidget('Dialog-Create Microstructure').resize(210, 236)
+findWidget('Dialog-Create Microstructure:widget_GTK_RESPONSE_OK').clicked()
+checkpoint meshable button set
+checkpoint microstructure page sensitized
+checkpoint pixel page updated
+checkpoint active area status updated
+findWidget('OOF2:Microstructure Page:Pane').set_position(189)
+checkpoint pixel page sensitized
+checkpoint mesh bdy page updated
+checkpoint Field page sensitized
+checkpoint Materials page updated
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page sensitized
+checkpoint boundary page updated
+checkpoint skeleton selection page grouplist Element
+checkpoint skeleton selection page selection sensitized Element
+checkpoint skeleton selection page groups sensitized Element
+checkpoint skeleton selection page updated
+checkpoint Solver page sensitized
+checkpoint OOF.Microstructure.New
+findWidget('OOF2:Navigation:PrevHist').clicked()
+checkpoint Solver page sensitized
+checkpoint page installed Solver
+# Sensitization test.  MS defined.
+assert tests.sensitive(False)
+
+event(Gdk.EventType.BUTTON_PRESS,x= 5.5000000000000e+01,y= 7.0000000000000e+00,button=1,state=0,window=findWidget('OOF2:Navigation:PageMenu').get_window())
+checkpoint toplevel widget mapped chooserPopup-PageMenu
+findMenu(findWidget('chooserPopup-PageMenu'), ['Materials']).activate() # MenuItemLogger
+deactivatePopup('chooserPopup-PageMenu') # MenuItemLogger
+checkpoint Materials page updated
+checkpoint page installed Materials
+findWidget('OOF2:Materials Page:Pane').set_position(289)
+findWidget('OOF2:Materials Page:Pane:Material:New').clicked()
+checkpoint toplevel widget mapped Dialog-New material
+findWidget('Dialog-New material').resize(196, 122)
+findWidget('Dialog-New material:widget_GTK_RESPONSE_OK').clicked()
+checkpoint Materials page updated
+checkpoint OOF.Material.New
+findWidget('OOF2:Materials Page:Pane:Material:Assign').clicked()
+checkpoint toplevel widget mapped Dialog-Assign material material to pixels
+findWidget('Dialog-Assign material material to pixels').resize(235, 122)
+event(Gdk.EventType.BUTTON_PRESS,x= 5.3000000000000e+01,y= 1.3000000000000e+01,button=1,state=0,window=findWidget('Dialog-Assign material material to pixels:pixels').get_window())
+checkpoint toplevel widget mapped chooserPopup-pixels
+findMenu(findWidget('chooserPopup-pixels'), ['<every>']).activate() # MenuItemLogger
+deactivatePopup('chooserPopup-pixels') # MenuItemLogger
+findWidget('Dialog-Assign material material to pixels:widget_GTK_RESPONSE_OK').clicked()
+checkpoint OOF.Material.Assign
+findWidget('OOF2:Materials Page:Pane:Property:PropertyScroll:PropertyTree').get_selection().select_path(Gtk.TreePath([0]))
+checkpoint Materials page updated
+checkpoint property selected
+findWidget('OOF2:Materials Page:Pane:Property:PropertyScroll:PropertyTree').expand_row(Gtk.TreePath([1]), open_all=False)
+event(Gdk.EventType.BUTTON_RELEASE,x= 1.1000000000000e+01,y= 2.5000000000000e+01,button=1,state=256,window=findWidget('OOF2:Materials Page:Pane:Property:PropertyScroll:PropertyTree').get_window())
+findWidget('OOF2:Materials Page:Pane:Property:PropertyScroll:PropertyTree').expand_row(Gtk.TreePath([1, 0]), open_all=False)
+event(Gdk.EventType.BUTTON_RELEASE,x= 2.9000000000000e+01,y= 4.3000000000000e+01,button=1,state=256,window=findWidget('OOF2:Materials Page:Pane:Property:PropertyScroll:PropertyTree').get_window())
+findWidget('OOF2:Materials Page:Pane:Property:PropertyScroll:PropertyTree').get_selection().select_path(Gtk.TreePath([1, 0, 0]))
+checkpoint Materials page updated
+checkpoint property deselected
+checkpoint Materials page updated
+checkpoint property selected
+event(Gdk.EventType.BUTTON_RELEASE,x= 7.9000000000000e+01,y= 6.6000000000000e+01,button=1,state=256,window=findWidget('OOF2:Materials Page:Pane:Property:PropertyScroll:PropertyTree').get_window())
+findWidget('OOF2:Materials Page:Pane:Property:Add').clicked()
+checkpoint property selected
+checkpoint Materials page updated
+checkpoint OOF.Material.Add_property
+findWidget('OOF2:Materials Page:Pane:Property:PropertyScroll:PropertyTree').expand_row(Gtk.TreePath([1, 4]), open_all=False)
+event(Gdk.EventType.BUTTON_RELEASE,x= 2.7000000000000e+01,y= 1.6900000000000e+02,button=1,state=256,window=findWidget('OOF2:Materials Page:Pane:Property:PropertyScroll:PropertyTree').get_window())
+findWidget('OOF2:Materials Page:Pane:Property:PropertyScroll:PropertyTree').get_selection().select_path(Gtk.TreePath([1, 4, 0]))
+checkpoint Materials page updated
+checkpoint property deselected
+checkpoint Materials page updated
+checkpoint property selected
+event(Gdk.EventType.BUTTON_RELEASE,x= 8.3000000000000e+01,y= 1.8500000000000e+02,button=1,state=256,window=findWidget('OOF2:Materials Page:Pane:Property:PropertyScroll:PropertyTree').get_window())
+findWidget('OOF2:Materials Page:Pane:Property:Add').clicked()
+checkpoint property selected
+checkpoint Materials page updated
+checkpoint OOF.Material.Add_property
+# Create a Skeleton
+event(Gdk.EventType.BUTTON_PRESS,x= 5.7000000000000e+01,y= 1.7000000000000e+01,button=1,state=0,window=findWidget('OOF2:Navigation:PageMenu').get_window())
+checkpoint toplevel widget mapped chooserPopup-PageMenu
+findMenu(findWidget('chooserPopup-PageMenu'), ['Skeleton']).activate() # MenuItemLogger
+deactivatePopup('chooserPopup-PageMenu') # MenuItemLogger
+checkpoint page installed Skeleton
+findWidget('OOF2:Skeleton Page:Pane').set_position(417)
+checkpoint skeleton page sensitized
+checkpoint skeleton page sensitized
+findWidget('OOF2:Skeleton Page:New').clicked()
+checkpoint toplevel widget mapped Dialog-New skeleton
+findWidget('Dialog-New skeleton').resize(346, 254)
+findWidget('Dialog-New skeleton:widget_GTK_RESPONSE_OK').clicked()
+checkpoint skeleton selection page groups sensitized Element
+checkpoint skeleton selection page groups sensitized Element
+checkpoint skeleton selection page groups sensitized Element
+checkpoint skeleton selection page selection sensitized Element
+checkpoint skeleton selection page updated
+checkpoint skeleton selection page groups sensitized Element
+checkpoint skeleton page sensitized
+checkpoint mesh bdy page updated
+checkpoint Field page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page sensitized
+checkpoint boundary page updated
+checkpoint skeleton page info updated
+checkpoint skeleton selection page grouplist Element
+checkpoint skeleton page info updated
+checkpoint skeleton selection page selection sensitized Element
+checkpoint Solver page sensitized
+checkpoint OOF.Skeleton.New
+checkpoint skeleton selection page groups sensitized Element
+checkpoint skeleton selection page updated
+checkpoint skeleton page sensitized
+
+# Sensitization check.  MS, material, & skeleton defined.
+event(Gdk.EventType.BUTTON_PRESS,x= 9.1000000000000e+01,y= 2.1000000000000e+01,button=1,state=0,window=findWidget('OOF2:Navigation:PageMenu').get_window())
+checkpoint toplevel widget mapped chooserPopup-PageMenu
+findMenu(findWidget('chooserPopup-PageMenu'), ['Solver']).activate() # MenuItemLogger
+deactivatePopup('chooserPopup-PageMenu') # MenuItemLogger
+checkpoint Solver page sensitized
+checkpoint page installed Solver
+assert tests.sensitive(False)
+
+# Create a Mesh
+event(Gdk.EventType.BUTTON_PRESS,x= 6.0000000000000e+01,y= 2.1000000000000e+01,button=1,state=0,window=findWidget('OOF2:Navigation:PageMenu').get_window())
+checkpoint toplevel widget mapped chooserPopup-PageMenu
+findMenu(findWidget('chooserPopup-PageMenu'), ['FE Mesh']).activate() # MenuItemLogger
+deactivatePopup('chooserPopup-PageMenu') # MenuItemLogger
+checkpoint mesh page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page sensitized
+checkpoint page installed FE Mesh
+findWidget('OOF2:FE Mesh Page:Pane:leftpane').set_position(130)
+findWidget('OOF2:FE Mesh Page:New').clicked()
+checkpoint toplevel widget mapped Dialog-Create a new mesh
+findWidget('Dialog-Create a new mesh').resize(299, 244)
+findWidget('Dialog-Create a new mesh:widget_GTK_RESPONSE_OK').clicked()
+checkpoint mesh bdy page updated
+checkpoint mesh bdy page updated
+checkpoint Field page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint OOF.Mesh.New
+
+# Sensitization check.  mesh defined.
+event(Gdk.EventType.BUTTON_PRESS,x= 9.9000000000000e+01,y= 1.5000000000000e+01,button=1,state=0,window=findWidget('OOF2:Navigation:PageMenu').get_window())
+checkpoint toplevel widget mapped chooserPopup-PageMenu
+findMenu(findWidget('chooserPopup-PageMenu'), ['Solver']).activate() # MenuItemLogger
+deactivatePopup('chooserPopup-PageMenu') # MenuItemLogger
+checkpoint Solver page sensitized
+checkpoint page installed Solver
+assert tests.sensitive(False)
+
+# Fields and Equations
+event(Gdk.EventType.BUTTON_PRESS,x= 8.1000000000000e+01,y= 1.9000000000000e+01,button=1,state=0,window=findWidget('OOF2:Navigation:PageMenu').get_window())
+checkpoint toplevel widget mapped chooserPopup-PageMenu
+findMenu(findWidget('chooserPopup-PageMenu'), ['Fields & Equations']).activate() # MenuItemLogger
+deactivatePopup('chooserPopup-PageMenu') # MenuItemLogger
+checkpoint Field page sensitized
+checkpoint Field page sensitized
+checkpoint page installed Fields & Equations
+findWidget('OOF2:Fields & Equations Page:HPane').set_position(470)
+findWidget('OOF2:Fields & Equations Page:HPane:Fields:Displacement defined').clicked()
+checkpoint Field page sensitized
+checkpoint Field page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint Solver page sensitized
+checkpoint OOF.Subproblem.Field.Define
+findWidget('OOF2:Fields & Equations Page:HPane:Fields:Displacement active').clicked()
+checkpoint Field page sensitized
+checkpoint Field page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint OOF.Subproblem.Field.Activate
+findWidget('OOF2:Fields & Equations Page:HPane:Fields:Displacement in-plane').clicked()
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint OOF.Mesh.Field.In_Plane
+findWidget('OOF2:Fields & Equations Page:HPane:Equations:Force_Balance active').clicked()
+checkpoint Field page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page subproblems sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint OOF.Subproblem.Equation.Activate
+
+# Sensitization check.  fields & equations defined.
+event(Gdk.EventType.BUTTON_PRESS,x= 7.4000000000000e+01,y= 1.7000000000000e+01,button=1,state=0,window=findWidget('OOF2:Navigation:PageMenu').get_window())
+checkpoint toplevel widget mapped chooserPopup-PageMenu
+findMenu(findWidget('chooserPopup-PageMenu'), ['Solver']).activate() # MenuItemLogger
+deactivatePopup('chooserPopup-PageMenu') # MenuItemLogger
+checkpoint Solver page sensitized
+checkpoint page installed Solver
+assert tests.sensitive(False)
+
+# Set the solver
+findWidget('OOF2:Solver Page:VPane:Subproblems:SubproblemScroll:SubproblemList').get_selection().select_path(Gtk.TreePath([0]))
+checkpoint Solver page sensitized
+tree=findWidget('OOF2:Solver Page:VPane:Subproblems:SubproblemScroll:SubproblemList')
+column = tree.get_column(3)
+tree.row_activated(Gtk.TreePath([0]), column)
+checkpoint toplevel widget mapped Dialog-Specify Solver
+findWidget('Dialog-Specify Solver').resize(414, 253)
+findWidget('Dialog-Specify Solver:widget_GTK_RESPONSE_OK').clicked()
+checkpoint Solver page sensitized
+checkpoint Field page sensitized
+checkpoint mesh page sensitized
+checkpoint Solver page sensitized
+checkpoint OOF.Subproblem.Set_Solver
+# Sensitization check.  solver set. "Solve" should be sensitized
+assert tests.sensitive(True)
+
+findMenu(findWidget('OOF2:MenuBar'), ['File', 'Save', 'Python_Log']).activate()
+checkpoint toplevel widget mapped Dialog-Python_Log
+findWidget('Dialog-Python_Log').resize(192, 122)
+findWidget('Dialog-Python_Log:filename').set_text('s')
+findWidget('Dialog-Python_Log:filename').set_text('se')
+findWidget('Dialog-Python_Log:filename').set_text('ses')
+findWidget('Dialog-Python_Log:filename').set_text('sess')
+findWidget('Dialog-Python_Log:filename').set_text('sessi')
+findWidget('Dialog-Python_Log:filename').set_text('sessio')
+findWidget('Dialog-Python_Log:filename').set_text('session')
+findWidget('Dialog-Python_Log:filename').set_text('session.')
+findWidget('Dialog-Python_Log:filename').set_text('session.l')
+findWidget('Dialog-Python_Log:filename').set_text('session.lo')
+findWidget('Dialog-Python_Log:filename').set_text('session.log')
+findWidget('Dialog-Python_Log:widget_GTK_RESPONSE_OK').clicked()
+checkpoint OOF.File.Save.Python_Log
+assert tests.filediff("session.log")
+findMenu(findWidget('OOF2:MenuBar'), ['File', 'Quit']).activate()
