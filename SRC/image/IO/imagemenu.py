177eb4c30baf0eda8bef054d9d2b8a9caf4775d0
Steve Langer <stephen.langer@nist.gov>
2021-04-23 17:54:10 -0400

    Removed a TODO that had already been done.

--- SRC/image/IO/imagemenu.py
+++ SRC/image/IO/imagemenu.py
@@ -199,8 +199,6 @@ def imageNameResolver(param, startname):
     msname = labeltree.makePath(param.group['microstructure'].value)[0]
     return whoville.getClass('Image').uniqueName([msname, imagename])
 
-## TODO GTK3: Fix copyImage. It calls write_pause() when not writing
-## if copying to a new microstructure.
 def copyImage(menuitem, image, microstructure, name):
     sourceimage = oofimage.getImage(image)
     immidge = sourceimage.clone(name)
