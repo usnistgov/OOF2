0b3ed570422166b9b94c32c1fe7f77f0c269dcb7
Steve Langer <stephen.langer@nist.gov>
2021-07-09 11:22:56 -0400

    Reduced the required version of pygobject, because it was too high for Ubuntu.
    Also tweaked the build directory name again. The change won't have any effect
    except on Macs.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ MAGICK_VERSION = "6.0"
 CAIROMM_VERSION = "1.12" # Don't know what the earliest acceptable version is.
 PANGO_VERSION = "1.40"
 PANGOCAIRO_VERSION = "1.40"
-PYGOBJECT_VERSION = "3.28"
+PYGOBJECT_VERSION = "3.26"
 
 # The make_dist script edits the following line when a distribution is
 # built.  Don't change it by hand.  On the git master branch,
