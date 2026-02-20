13744b1c66301717e76a0dd5ebc26429d35df370
Steve Langer <stephen.langer@nist.gov>
2026-02-19 22:04:24 -0500

    More debugging.
    oof2.in calls testRubberBand (in rubberband.C) before starting the GUI.
    
    Code to compile testRubberBand into its own libary is in CMakeLists.txt
    (two different files) and debugrubberband.C, but commented out in
    the CMakeLists.txt files.

---
+++ SRC/common/IO/GUI/debugrubberband.C
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <oofcanvasgui.h>
+
+// This was compiled into its own library for debugging.  To resurrect
+// it, search for and uncomment code containing oof2debug in the top
+// CMakeLists.txt and the one in SRC/common/IO/GUI.
+
+void testRubberBand() {
+  std::cerr << "testRubberBand in debugrubberband.C" << std::endl;
+  OOFCanvas::CanvasCurve *trail = new OOFCanvas::CanvasCurve();
+  std::cerr << "testRubberBand: trail=" << trail << std::endl;
+  std::cerr << "testRubberBand: calling setLineColor" << std::endl;
+  trail->setLineColor(OOFCanvas::red);
+  std::cerr << "testRubberBand: calling setLineWidthInPixels" << std::endl;
+  trail->setLineWidthInPixels(17);
+  std::cerr << "testRubberBand: done" << std::endl;
+}
