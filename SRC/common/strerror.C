a7f36159e204db7d6f699bf001a8ea4d96051609
Steve Langer <stephen.langer@nist.gov>
2026-03-06 21:17:54 -0500

    Better handling for strerror_r's pecularities.
    THe previous hack didn't work.  Here's an attempt to do it right.
    This works on Mac, will try on Linux next.

---
+++ SRC/common/strerror.C
@@ -0,0 +1,36 @@
+// -*- C++ -*-
+
+/* This software was produced by NIST, an agency of the U.S. government,
+ * and by statute is not subject to copyright in the United States.
+ * Recipients of this software assume all responsibilities associated
+ * with its operation, modification and maintenance. However, to
+ * facilitate maintenance we ask that before distributing modified
+ * versions of this software, you first contact the authors at
+ * oof_manager@nist.gov. 
+ */
+
+#include <oofconfig.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include "common/strerror.h"
+
+// Wrapper for strerror_r, which has a different return value on posix
+// and non-posix systesms.  CMake sets HAVE_POSIX_STRERROR_R in
+// oofconfig.h.
+
+// oof_strerror() returns the string representation of the current
+// error, which set in errno.  
+
+std::string oof_strerror() {
+  char buf[1000];
+#ifdef HAVE_POSIX_STRERROR_R
+  int status = strerror_r(errno, buf, sizeof(buf));
+#else  // not HAVE_POSIX_STRERROR_R
+  char *s = strerror_r(errno, buf, sizeof(buf));
+#endif // not HAVE_POSIX_STRERROR_R
+  return std::string(buf);
+}
+
+
+
