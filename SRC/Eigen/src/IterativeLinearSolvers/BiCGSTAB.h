0bc46f77f47f314c77e042b483c9e82e69f30b06
Steve Langer <stephen.langer@nist.gov>
2026-05-05 22:00:16 -0400

    Fixed a bug in the Eigen stabilized biconjugate gradient solver.
    It turned out that it was already fixed in their git repository,
    saving me the trouble of writing a bug report.  The conditional used
    to stop the iteration was not the same as the conditional used
    afterward to determine if it had converged, leading to spurious
    nonconvergence errors.

--- SRC/Eigen/src/IterativeLinearSolvers/BiCGSTAB.h
+++ SRC/Eigen/src/IterativeLinearSolvers/BiCGSTAB.h
@@ -17,8 +17,8 @@
 // IWYU pragma: private
 #include "./InternalHeaderCheck.h"
 
-#include "common/progress.h"	// OOF
-#include "common/tostring.h"	// OOF
+// #include "common/progress.h"	// OOF
+// #include "common/tostring.h"	// OOF
 
 namespace Eigen {
 
@@ -40,7 +40,6 @@ bool bicgstab(const MatrixType& mat, const Rhs& rhs, Dest& x, const Precondition
   typedef typename Dest::RealScalar RealScalar;
   typedef typename Dest::Scalar Scalar;
   typedef Matrix<Scalar, Dynamic, 1> VectorType;
-  RealScalar tol = tol_error;
   Index maxIters = iters;
 
   Index n = mat.cols();
@@ -54,6 +53,9 @@ bool bicgstab(const MatrixType& mat, const Rhs& rhs, Dest& x, const Precondition
     x.setZero();
     return true;
   }
+
+  RealScalar tol = tol_error * rhs_norm;
+
   Scalar rho(1);
   Scalar alpha(0);
   Scalar w(1);
@@ -187,6 +189,7 @@ struct traits<BiCGSTAB<MatrixType_, Preconditioner_> > {
  */
 template <typename MatrixType_, typename Preconditioner_>
 class BiCGSTAB : public IterativeSolverBase<BiCGSTAB<MatrixType_, Preconditioner_> > {
+ protected:
   typedef IterativeSolverBase<BiCGSTAB> Base;
   using Base::m_error;
   using Base::m_info;
